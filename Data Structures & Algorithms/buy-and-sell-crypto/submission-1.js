fff1e1e020d3e7199a559410cabba53d8a680a8c
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-05-21 05:37:31 +0900

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = Infinity;
+        let profit = 0;
+        for(let i = 0; i < prices.length; i++) {
+
+            if(prices[i] < min) min = prices[i];
+            if(prices[i] > min) {
+                profit = Math.max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
+}
