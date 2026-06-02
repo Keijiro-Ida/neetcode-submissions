28588e90dc71fe1ed2a6325f2925d293fdbcf402
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-06-03 06:02:00 +0900

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        const arr = [...stones];
+
+        while(arr.length > 1) {
+            arr.sort((a, b) => a - b);
+            const y = arr.pop();
+            const x = arr.pop();
+            
+            if(x != y) {
+                arr.push(y - x);
+            }
+        }
+        return arr.length === 0 ? 0: arr[0];
+    }
+}
