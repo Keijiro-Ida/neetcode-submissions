ff675595efb5e0c9bc348cf79b11a9a9f9d2d916
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-06-12 06:06:46 +0900

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        const result = [];
+
+        for(let i = 0; i <= n; i++) {
+            result[i] = this.countOne(i);
+        }
+        return result;
+    }
+
+    countOne(n) {
+        let count = 0;
+        while(n > 0) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+}
