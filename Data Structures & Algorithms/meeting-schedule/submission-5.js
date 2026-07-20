2c631421231a95e595d5ea0f6c2ed2d666713cb8
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-07-21 06:02:24 +0900

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.js
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        if(intervals.length < 1) return true;
+        const sorted = [...intervals].sort((a, b) => a.start - b.start);
+
+        for(let i = 1; i < intervals.length; i++) {
+            if(sorted[i].start < sorted[i - 1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
