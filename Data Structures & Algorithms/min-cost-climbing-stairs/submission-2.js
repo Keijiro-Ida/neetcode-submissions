fc5183442c74aa8a488a0399050820418afb739e
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-07-22 05:51:14 +0900

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        let prev1 = 0, prev2 = 0;
+
+        for(let i = 2; i <= cost.length; i++) {
+            const curr = Math.min(cost[i - 2] + prev2, cost[i - 1] + prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
