78ede6f1317f36c70488610f067171abf1a6344f
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-07-24 05:54:09 +0900

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} digits
+     * @return {number[]}
+     */
+    plusOne(digits) {
+
+        let add = 1;
+
+        for(let i = digits.length - 1; i >= 0; i--) {
+            
+            if(digits[i] != 9) {
+                digits[i] += add;
+                add = 0;
+                break;
+            }
+            
+            digits[i] = 0;
+        }
+        if(add === 1) {
+            digits.unshift(1);
+        }
+        return digits;
+    }
+}
