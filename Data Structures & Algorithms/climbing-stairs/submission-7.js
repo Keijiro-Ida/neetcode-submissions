7be44df137b41f908a41aa94ed2bf5722ec8f854
Keijiro-Ida <84495943+Keijiro-Ida@users.noreply.github.com>
2026-08-21 06:14:05 +0900

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if(n < 3) return n;
+        let prev1 = 2; let prev2 = 1;
+
+        for(let i = 3; i <= n; i++) {
+            const curr = prev1 + prev2;
+            const tmp = prev1;
+            prev1 = curr;
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+}
